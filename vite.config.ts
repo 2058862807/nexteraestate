f5fc502170c66e609cc820ebf270f57c1f7c029c
2058862807 <dustintylerjames@gmail.com>
2025-07-31 08:21:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import checker from "vite-plugin-checker";
-import tsconfigPaths from "vite-tsconfig-paths"; // Add this
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// Explicitly define __dirname for ES modules
+const __dirname = path.dirname(new URL(import.meta.url).pathname;
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths(), // Add this plugin
+    tsconfigPaths(), // Add path resolver plugin
     checker({
       typescript: true,
       eslint: {
