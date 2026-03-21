f06942626a246c9734f260c5b72a6a276272be9b
Dustin James <dustintylerjames@gmail.com>
2026-03-21 18:20:41 -0500

    Fix Tailwind directives and drizzle config

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,12 +1,10 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
+  dialect: 'postgresql',
   schema: './schema.ts',
   out: './drizzle',
-  driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
-  verbose: true,
-  strict: true,
-} satisfies Config;
+});
